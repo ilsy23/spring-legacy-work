747e128eb31f71712e253c47151be9463e5d83eb
hansolL <ilsysli@naver.com>
2023-10-13 17:17:17 +0900

    23. 10. 13. 수업

--- MyWeb/src/test/java/com/spring/myweb/freeboard/FreeBoardMapperTest.java
+++ MyWeb/src/test/java/com/spring/myweb/freeboard/FreeBoardMapperTest.java
@@ -1,8 +1,11 @@
 package com.spring.myweb.freeboard;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,10 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.spring.myweb.freeboard.dto.page.Page;
 import com.spring.myweb.freeboard.entity.FreeBoard;
 import com.spring.myweb.freeboard.mapper.IFreeBoardMapper;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class) // 테스트 환경을 만들어 주는 Junit5 객체 로딩
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
@@ -58,7 +60,10 @@ public class FreeBoardMapperTest {
 	void getListTest() {
 		
 		// when
-		List<FreeBoard> list = mapper.getList();
+		List<FreeBoard> list = mapper.getList(Page.builder()
+													.pageNo(4)
+													.amount(10)
+													.build());
 		for(FreeBoard board : list) {
 			System.out.println(board);
 			
@@ -66,7 +71,7 @@ public class FreeBoardMapperTest {
 		System.out.println("조회된 글의 개수: " + list.size());
 		
 		// then
-		assertEquals(list.size(), 11);
+		assertEquals(list.size(), 10);
 		
 	
 	}
@@ -128,7 +133,10 @@ public class FreeBoardMapperTest {
 		mapper.delete(bno);
 		
 		// then
-		assertEquals(mapper.getList().size(), 10);
+		assertEquals(mapper.getList(Page.builder()
+										.pageNo(4)
+										.amount(10)
+										.build()).size(), 10);
 		assertNull(mapper.getContent(bno));
 		
 	}
